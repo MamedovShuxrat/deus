b3054b8e3ad326ebdf0dc18f3295761b4e84fab9
Shuxrat Mamedov <lider_20_10@mail.ru>
2024-07-01 00:03:17 +0300

    added updateCircle.js

--- src/js/updateCircle.js
+++ src/js/updateCircle.js
@@ -3,17 +3,19 @@ const percentageData = [20, 35, 42, 55, 70, 85, 90];
 const dotsContainer = document.querySelector('.products__dots');
 const circleInner = document.querySelector('.products__circle_inner');
 const circleData = document.querySelector('.products__circle_data');
+const productPercentage = document.getElementById('productPercentage')
 
 function updateCircle(index) {
     const percentage = percentageData[index];
     circleData.textContent = `${percentage}%`;
+    productPercentage.textContent = percentage
     circleInner.style.background = `conic-gradient(rgb(57, 199, 236) 0%, rgb(57, 199, 236) ${percentage}%, #ffffff ${percentage}%)`;
 }
 
 let currentIndex = 0;
 updateCircle(currentIndex);
 
-function createDot(percentage) {
+function createDot(percentage, index) {
     const dot = document.createElement('span');
     dot.className = 'products__dot';
     dot.style.background = `conic-gradient(rgb(57, 199, 236) 0%, rgb(57, 199, 236) ${percentage}%, #ffffff ${percentage}%)`;
@@ -24,12 +26,35 @@ function createDot(percentage) {
     dot.appendChild(dotData);
     dotsContainer.appendChild(dot);
 }
-percentageData.forEach(createDot);
 
-// setInterval(() => {
-//     currentIndex = (currentIndex + 1) % percentageData.length;
-//     updateCircle(currentIndex);
-// }, 2000);
-console.log('work');
+percentageData.forEach((percentage, index) => createDot(percentage, index));
 
+function moveSlider(direction) {
+    if (direction === 'left') {
+        currentIndex = (currentIndex - 1 + percentageData.length) % percentageData.length;
+    } else if (direction === 'right') {
+        currentIndex = (currentIndex + 1) % percentageData.length;
+    }
+    updateCircle(currentIndex);
+    updateDots();
+}
+
+function updateDots() {
+    const dots = document.querySelectorAll('.products__dot');
+    dots.forEach((dot, index) => {
+        const percentage = percentageData[(currentIndex + index) % percentageData.length];
+        dot.style.background = `conic-gradient(rgb(57, 199, 236) 0%, rgb(57, 199, 236) ${percentage}%, #ffffff ${percentage}%)`;
+    });
+}
+
+
+document.getElementById('left-btn-pr').addEventListener('click', () => {
+    console.log('Left button clicked!');
+    moveSlider('left');
+});
+
+document.getElementById('right-btn-pr').addEventListener('click', () => {
+    console.log('Right button clicked!');
+    moveSlider('right');
+});
 
